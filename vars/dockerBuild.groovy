a42039fdd942b068b69a299eedb75ca8d578c6a6
vikash <vikash@gmail.com>
2023-03-18 15:13:14 +0530

    static code anlysyis shared lib added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,16 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
+    
+//     sh """
+//      docker image build -t ${hubUser}/${project} . 
+//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     docker image build -t ${hubUser}/${project} . 
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+     docker build -t ${ecr_repoName} .
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
