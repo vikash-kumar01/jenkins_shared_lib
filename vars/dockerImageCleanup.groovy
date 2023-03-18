d393335bd5c30d6f6d27b08d3c564fb95988dff3
vikash <vikash@gmail.com>
2023-03-18 15:27:22 +0530

    static code anlysyis shared lib added

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,7 +1,15 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
+    
+//     sh """
+//      docker rmi ${hubUser}/${project}:${ImageTag}
+//      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     docker rmi ${hubUser}/${project}:${ImageTag}
-     docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+     docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -14,7 +14,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${${region}}.amazonaws.com
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker push ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
