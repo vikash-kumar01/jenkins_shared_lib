7b7ea78e3e40892cbb40f99f25f1b1dfae7ebd11
vikash <vikash@gmail.com>
2023-03-18 15:19:06 +0530

    static code anlysyis shared lib added

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,11 +1,20 @@
-def call(String project, String ImageTag, String hubUser){
-    withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"   
+// def call(String project, String ImageTag, String hubUser){
+//     withCredentials([usernamePassword(
+//             credentialsId: "docker",
+//             usernameVariable: "USER",
+//             passwordVariable: "PASS"
+//     )]) {
+//         sh "docker login -u '$USER' -p '$PASS'"
+//     }
+//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
+//     sh "docker image push ${hubUser}/${project}:latest"   
+// }
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${${region}}.amazonaws.com
+     docker push ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
 }
--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,15 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
-    sh """   
-     trivy image ${hubUser}/${project}:latest > scan.txt
-     cat scan.txt
+//     sh """   
+//      trivy image ${hubUser}/${project}:latest > scan.txt
+//      cat scan.txt
+//     """
+// }
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    sh """
+    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+    cat scan.txt
     """
 }
