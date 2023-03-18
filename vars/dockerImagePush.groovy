a817233dc1c5d1981559dd1f7a26d3f0dea759f8
vikash <vikash@gmail.com>
2023-03-18 15:51:43 +0530

    static code anlysyis shared lib added

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -15,6 +15,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
