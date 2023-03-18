874578311993abd33a7398d82c0dbab978d38a71
vikash <vikash@gmail.com>
2023-03-18 14:18:06 +0530

    static code anlysyis shared lib added

---
+++ vars/dockerImageScan.groovy
@@ -0,0 +1,7 @@
+def call(String project, String ImageTag, String hubUser){
+    
+    sh """   
+     trivy image ${hubUser}/${project} ${hubUser}/${project}:latest > scan.txt
+     cat scan.txt
+    """
+}
